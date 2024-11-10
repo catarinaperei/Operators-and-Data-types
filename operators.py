6bcac64c24fb8f934b96ffff85aeee290aa17ef1
catarinaperei <smlilwana@gmail.com>
2024-11-10 14:49:53 +0200

    commit Initial

---
+++ operators.py
@@ -0,0 +1,11 @@
+name="Catarina"
+age=15
+weight=48.7
+student=True
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(student))
+age=str(age)
+print(type(age))
+
---
+++ print.py
@@ -0,0 +1,15 @@
+star1=22
+star2=44
+star3=33
+star4=11
+star5=55
+star6=12
+star7=16
+print("the subtraction is",star1-star2)
+print("the division is",star1/star2)
+print("the additon is",star3+star4)
+print("the multiplication is",star3*star4)
+print("the floor division",star4//star5)
+print("the modulus is",star4%star5)
+print("the exponential is",star6**star7)
+
